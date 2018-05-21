93442015752bb31b04da240116447016f4fd692f
iTzFreeHD <jaaptijmes58@gmail.com>
2018-05-21 19:01:19 +0200

    Add files via upload

---
+++ LobbySystem - iTzFreeHDv2/src/iTzFreeHD/LB/Listeners.php
@@ -0,0 +1,217 @@
+<?php
+namespace iTzFreeHD\LB;
+
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\item\Item;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as c;
+
+class Listeners implements Listener {
+
+    private $plugin;
+
+    public function __construct(LobbySystem $plugin) {
+        $this->plugin = $plugin;
+        $this->plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+    }
+
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        @mkdir($this->plugin->getDataFolder().'/player');
+        $cfg = new Config($this->plugin->getDataFolder() . '/Items.yml', Config::YAML);
+        $JoinMenu = $cfg->get('JoinMenu');
+
+        $pcfg = new Config($this->plugin->getDataFolder().'/player/'.$event->getPlayer()->getName().'.yml',Config::YAML);
+        $pcfg->set("Menu", $JoinMenu);
+        $pcfg->save();
+
+        $this->setItems($event->getPlayer(), $JoinMenu);
+    }
+
+    public function onInteract(PlayerInteractEvent $event)
+    {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $in = $event->getPlayer()->getInventory()->getItemInHand()->getCustomName();
+        $inv = $player->getInventory();
+
+        $cfg = new Config($this->plugin->getDataFolder()."/Items.yml", Config::YAML);
+        $pcfg = new Config($this->plugin->getDataFolder().'/player/'.$event->getPlayer()->getName().'.yml',Config::YAML);
+        $menu = $pcfg->get('Menu');
+        $fdata = $cfg->get($menu);
+        foreach ($fdata as $gdata) {
+            if ($gdata['name'] == $in) {
+                $sdata = $cfg->get($menu);
+                $data = $sdata[c::clean($in)];
+
+                if ($in == $data['name']) {
+                    $ac = explode(':', $data['action']);
+                    if ($data['permissions'] === "") {
+
+                        if ($ac[0] === 'msg') {
+                            $player->sendMessage($ac[1]);
+                        } elseif ($ac[0] === 'cmd'){
+                            $this->plugin->getServer()->dispatchCommand($event->getPlayer(), $ac[1]);
+                        } elseif ($ac[0] === 'menu') {
+                            $this->setItems($player, $ac[1]);
+                        } elseif ($ac[0] === 'tp') {
+                            $coords = explode('.', $ac[0]);
+                            if ($ac[1] == 'spawn') {
+
+                                $x = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
+                                $y = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
+                                $z = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
+                                $event->getPlayer()->teleport(new Vector3($x, $y, $z), 0, 0);
+                            } else {
+                                $event->getPlayer()->teleport(new Position($coords[0], $coords[1], $coords[2], $coords[3]), 0, 0);
+                            }
+
+                        }
+
+
+                    } else {
+                        if ($player->hasPermission($data['permissions'])) {
+                            if ($ac[0] === 'msg') {
+                                $player->sendMessage($ac[1]);
+                            } elseif ($ac[0] === 'cmd'){
+                                $this->plugin->getServer()->dispatchCommand($event->getPlayer(), $ac[1]);
+                            } elseif ($ac[0] === 'menu') {
+                                $this->setItems($player, $ac[1]);
+                            } elseif ($ac[0] === 'tp') {
+                                $coords = explode('.', $ac[0]);
+                                if ($ac[1] == 'spawn') {
+
+                                    $x = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
+                                    $y = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
+                                    $z = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
+                                    $event->getPlayer()->teleport(new Vector3($x, $y, $z), 0, 0);
+                                } else {
+                                    $event->getPlayer()->teleport(new Position($coords[0], $coords[1], $coords[2], $coords[3]), 0, 0);
+                                }
+
+                            }
+                        } else {
+                            $player->sendMessage($cfg->get('NoPermission'));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //no Viod
+    public function onMove(PlayerMoveEvent $event) {
+        $cfg = new Config($this->plugin->getDataFolder() . '/Items.yml', Config::YAML);
+        if ($cfg->get('noVoid') === true) {
+            $py = $event->getPlayer()->getY();
+
+            if ($py < 5) {
+                $event->getPlayer()->teleport($this->plugin->getServer()->getDefaultLevel()->getSafeSpawn(), 0, 0);
+            }
+        }
+
+
+    }
+
+
+    //Schaden
+    public function onDamage(EntityDamageEvent $event)
+    {
+        $cfg = new Config($this->plugin->getDataFolder() . '/Items.yml', Config::YAML);
+        if ($cfg->get('noDamage') === true) {
+            $event->setCancelled(true);
+        }
+
+    }
+
+    //noDrop
+    public function onDrop(PlayerDropItemEvent $event)
+    {
+        $cfg = new Config($this->plugin->getDataFolder() . '/Items.yml', Config::YAML);
+        if ($cfg->get('noDrop') === true) {
+            $event->setCancelled();
+        }
+
+    }
+
+    //noHunger
+    public function onHunger(PlayerExhaustEvent $event)
+    {
+        $cfg = new Config($this->plugin->getDataFolder() . '/Items.yml', Config::YAML);
+        if ($cfg->get('noHunger') === true) {
+            $event->setCancelled(true);
+        }
+
+    }
+
+    //Build
+    public function onPlace(BlockPlaceEvent $event)
+    {
+        $name = $event->getPlayer()->getName();
+        if (!in_array($name, $this->plugin->buildmode)) {
+
+            $event->setCancelled();
+
+        }
+    }
+
+
+    public function onBreak(BlockBreakEvent $event)
+    {
+        $name = $event->getPlayer()->getName();
+        if (!in_array($name, $this->plugin->buildmode)) {
+
+            $event->setCancelled();
+
+        }
+    }
+
+    public function noInvMove(InventoryTransactionEvent $event)
+    {
+        $cfg = new Config($this->plugin->getDataFolder() . '/Items.yml', Config::YAML);
+        foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+            $build = $cfg->get('Build-Perms');
+            if ($player->hasPermission($build)) {
+
+            }else {
+                $event->setCancelled();
+            }
+        }
+    }
+
+    //SetItem
+    public function setItems(Player $player, $menu)
+    {
+        $inv = $player->getInventory();
+        $inv->clearAll();
+
+        $cfg = new Config($this->plugin->getDataFolder()."/Items.yml", Config::YAML);
+        $sdata = $cfg->get($menu);
+
+        foreach ($sdata as $data) {
+            $id = explode(':', $data['id']);
+
+            $item = Item::get($id[0], $id[1], $id[2]);
+            $item->setCustomName($data['name']);
+
+            $inv->setItem($data['slot'], $item);
+        }
+        $pcfg = new Config($this->plugin->getDataFolder().'/player/'.$player->getName().'.yml',Config::YAML);
+        $pcfg->set("Menu", $menu);
+        $pcfg->save();
+
+    }
+}
---
+++ LobbySystem - iTzFreeHDv2/src/iTzFreeHD/LB/LobbySystem.php
@@ -0,0 +1,143 @@
+<?php
+namespace iTzFreeHD\LB;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as c;
+use pocketmine\utils\Config;
+
+
+class LobbySystem extends PluginBase {
+
+    public $p;
+    public $cfg;
+    public $buildmode = array('CONSOLE');
+    public $Listeners;
+
+    public function onEnable()
+    {
+        $this->deleteDirectory($this->getDataFolder()."/player/");
+        $this->getLogger()->info($this->p."Wurde erfolgreich gestartet");
+        @mkdir($this->getDataFolder());
+        $this->initConfig();
+        $this->cfg = new Config($this->getDataFolder() . '/Items.yml', Config::YAML);
+
+        $this->Listeners = new Listeners($this);
+    }
+
+
+    public function initConfig()
+    {
+
+
+        if (!file_exists($this->getDataFolder() . '/Items.yml')) {
+            $cfg = new Config($this->getDataFolder() . '/Items.yml', Config::YAML);
+            //JoinItems
+            $cfg->set('Build-Perms', 'lobby.build');
+            $cfg->set('noHunger', true);
+            $cfg->set('noDamage', true);
+            $cfg->set('noDrop', true);
+            $cfg->set('noVoid', true);
+            $cfg->set('NoPermission', '§cDu hast keine Rechte');
+            $cfg->set('JoinMenu', "Main");
+            $cfg->set('Main', []);
+            $cfg->set('Info', []);
+            $cfg->save();
+            $cfg->reload();
+
+            $items = $cfg->get('Main');
+
+            $items['Teleporter'] = ['name' => '§6Teleporter', 'id' => '345:0:1', 'slot' => 3, 'action' => 'menu:Info', 'permissions' => ''];
+            $items['Build'] = ['name' => '§6Build', 'id' => '2:0:1', 'slot' => 5, 'action' => 'cmd:build', 'permissions' => 'lobby.build'];
+            $items['Tools'] = ['name' => '§6Tools', 'id' => '396:0:1', 'slot' => 8, 'action' => 'cmd:freeparticle', 'permissions' => ''];
+
+            $cfg->set('Main', $items);
+
+            //InfoItem
+
+            $iitems = $cfg->get('Info');
+
+            $iitems['Info'] = ['name' => '§bInfo', 'id' => '340:0:1', 'slot' => 0, 'action' => 'msg: §bGehe auf github.com/Hyroxing um mehr Infos zu den Einstellungen zu erhalten', 'permissions' => 'lobby.info'];
+            $iitems['Spawn'] = ['name' => '§6Spawn', 'id' => '378:0:1', 'slot' => 4, 'action' => 'tp:spawn', 'permissions' => ''];
+            $iitems['Exit'] = ['name' => '§cExit', 'id' => '351:1:1', 'slot' => 8, 'action' => 'menu:Main', 'permissions' => ''];
+
+            $cfg->set('Info', $iitems);
+            $cfg->save();
+            $this->reloadConfig();
+            unlink($this->getDataFolder()."config.yml");
+        }
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args):bool {
+        $name = $sender->getName();
+        $cfg = new Config($this->getDataFolder() . '/Items.yml', Config::YAML);
+        $build = $cfg->get('Build-Perms');
+        if ($cmd->getName() == "build" && $sender->hasPermission($build)) {
+            if (!in_array($name, $this->buildmode)) {
+
+                $this->buildmode[] = $name;
+                $sender->sendMessage(c::GREEN . "Du kannst nun bauen.");
+
+            } else {
+
+                unset($this->buildmode[array_search($name, $this->buildmode)]);
+
+                $sender->sendMessage(c::RED . "Du kannst nun nicht mehr bauen.");
+
+            }
+
+        }
+
+        if ($cmd->getName() == "lobby") {
+
+            if ($sender instanceof Player) {
+                $spawn = $this->getServer()->getDefaultLevel()->getSafeSpawn();
+                $this->getServer()->getDefaultLevel()->loadChunk($spawn->getFloorX(), $spawn->getFloorZ());
+                $sender->teleport($spawn, 0, 0);
+
+            }
+
+
+        }
+
+        if ($cmd->getName() == "lbmain") {
+
+            if ($sender instanceof Player) {
+                if ($this->Listeners instanceof Listeners) {
+                    $cfg = new Config($this->getDataFolder() . '/Items.yml', Config::YAML);
+                    $JoinMenu = $cfg->get('JoinMenu');
+
+                    $pcfg = new Config($this->getDataFolder().'/player/'.$sender->getName().'.yml',Config::YAML);
+                    $pcfg->set("Menu", $JoinMenu);
+                    $pcfg->save();
+
+                    $this->Listeners->setItems($sender, $JoinMenu);
+                }
+
+            }
+
+
+        }
+        return true;
+    }
+
+    function deleteDirectory($dir) {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir."/".$object) == "dir"){
+                        rmdir($dir."/".$object);
+                    }else{
+                        unlink($dir."/".$object);
+                    }
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
+}
